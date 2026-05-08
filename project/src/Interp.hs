424bb0a78194038161d4125343771bd7b0b6121d
Walaz <wz57@illinois.edu>
2026-05-08 15:47:27 -0500

    feat: add primitive operation support

--- project/src/Interp.hs
+++ project/src/Interp.hs
@@ -14,6 +14,7 @@ eval (EVar x)    env = case M.lookup x env of
 eval (EPack t a) _   = VPack t a []
 eval (ELam ps b) env = VClos ps b env
 eval (EAp f x)   env = apply (eval f env) (eval x env)
+eval (EBinop n)  _   = VPrim n []
 eval _           _   = error "Interp: eval not yet implemented for this Expr"
 
 apply :: Value -> Value -> Value
@@ -23,8 +24,27 @@ apply (VClos (p:ps) body env) v =
 apply (VClos [] _ _) _ =
   error "apply: closure already saturated"
 apply (VPack t a args) v = VPack t a (args ++ [v])
+apply (VPrim n args) v =
+  let args' = args ++ [v]
+  in if length args' == 2 then applyPrim n args' else VPrim n args'
 apply other _ =
   error ("apply: cannot apply non-function value: " ++ show other)
+
+applyPrim :: Name -> [Value] -> Value
+applyPrim "+"  [VInt a,  VInt b]  = VInt  (a + b)
+applyPrim "-"  [VInt a,  VInt b]  = VInt  (a - b)
+applyPrim "*"  [VInt a,  VInt b]  = VInt  (a * b)
+applyPrim "/"  [VInt a,  VInt b]  = VInt  (a `div` b)
+applyPrim "<"  [VInt a,  VInt b]  = VBool (a <  b)
+applyPrim "<=" [VInt a,  VInt b]  = VBool (a <= b)
+applyPrim ">"  [VInt a,  VInt b]  = VBool (a >  b)
+applyPrim ">=" [VInt a,  VInt b]  = VBool (a >= b)
+applyPrim "==" [VInt a,  VInt b]  = VBool (a == b)
+applyPrim "~=" [VInt a,  VInt b]  = VBool (a /= b)
+applyPrim "&"  [VBool a, VBool b] = VBool (a && b)
+applyPrim "|"  [VBool a, VBool b] = VBool (a || b)
+applyPrim n    args               =
+  error ("applyPrim: bad args for " ++ n ++ ": " ++ show args)
 
 -- Use this function as your top-level entry point so you don't break `app/Main.hs`
 
